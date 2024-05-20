2a191b0355b017165a00ae97f3a9b8ab2d54c98a
ImDanielGitHub <134576019+ImDanielGitHub@users.noreply.github.com>
2024-05-21 04:54:06 +1200

    Working Version, about to start SearchInput

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,15 +1,41 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, FlatList, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FlatList } from "react-native-gesture-handler";
-
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-primary">
       <FlatList
-        data={[{ id: 1 }]}
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => <Text>{item.id}</Text>}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Daniel
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.cards}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+            {/* //TODO: This is a search funtion for searching upcoming events map
+            //could be placed here instead with the events showing at the bottom */}
+            <SearchInput />
+          </View>
+        )}
       />
     </SafeAreaView>
   );
